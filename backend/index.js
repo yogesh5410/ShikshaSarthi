cf16221363e54ddb3efb70abb8cf87a01fe8ae40
priyanshu-vishwakaram-hue <pvish8825@gmail.com>
2026-02-07 04:03:10 +0530

    audio setup

--- backend/index.js
+++ backend/index.js
@@ -45,6 +45,6 @@ app.use("/classes", classRoutes);
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
